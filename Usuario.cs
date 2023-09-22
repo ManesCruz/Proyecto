d75420f03738c312f438fc81613ce910eb175b01
ManesCruz <142680543+ManesCruz@users.noreply.github.com>
2023-09-21 21:10:24 -0500

    Update Usuario.cs
    usuario class

--- Usuario.cs
+++ Usuario.cs
@@ -8,16 +8,109 @@ namespace Proyecto
 {
     internal class Usuario
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public double Weight { get; set; }
-        public double Height { get; set; }
-        public string PhysicalLevel { get; set; }
-        public string Objective { get; set; }
-        public string Genre { get; set; }
-        public string UnitType { get; set; }
+        private string _name;
+        public string Name 
+        { 
+            get
+            {
+               return _name;        
+            } 
+            set
+            {
+                _name = value;
+            } 
+        }
+        
+        private int age;
+        public int Age 
+        {
+            get
+            {
+              return _age;  
+            }
+            set
+            {
+               _age = value;   
+            } 
+        }
 
+        private double _weight; 
+        public double Weight 
+        { 
+            get
+            {
+              return _weight;    
+            } 
+            set
+            {
+               _weight = value;
+            } 
+        }
+
+        private double _height;
+        public double Height 
+        { 
+            get
+            {
+             return _height;        
+            } 
+            set
+            {
+              _height = value;  
+            } 
+        }
+
+        private string _physicalLevel;
+        public string PhysicalLevel 
+        { 
+            get
+            {
+               return _physicalLevel;        
+            } 
+            set
+            {
+               _physicalLevel = value;  
+            } 
+        }
+
+        private string _objetive;
+        public string Objective 
+        { 
+            get
+            {
+               return _objetive;        
+            } 
+            set
+            {
+                _objetive = value;
+            } 
+        }
+
+        private string _genre;
+        public string Genre 
+        { 
+            get
+            {
+              return _genre;  
+            } 
+            set
+            {
+                _genre = value;
+            } 
+        }
+
+        private string _unitType;
+        public string UnitType 
+        { 
+            get
+            {
+              return _unitType;  
+            } 
+            set
+            {
+                _unitType = value;
+            } 
+        }
     }
-
 }
 
